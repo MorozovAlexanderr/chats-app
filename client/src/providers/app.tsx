e2badf0c31094c3c9a7dda1ac8f0c58457eee801
Alexander Morozov <amorozov@taker.io>
2024-02-07 16:16:11 +0200

    client: add error boundary

--- client/src/providers/app.tsx
+++ client/src/providers/app.tsx
@@ -1,6 +1,25 @@
 import { PropsWithChildren } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
+import Button from '@/components/Elements/Button';
+
+const ErrorFallback = () => {
+  return (
+    <div
+      className="flex h-screen w-screen flex-col items-center justify-center"
+      role="alert"
+    >
+      <h2 className="text-2xl">Ooops, something went wrong :( </h2>
+      <Button
+        className="mt-6"
+        onClick={() => window.location.assign(window.location.origin)}
+      >
+        Refresh
+      </Button>
+    </div>
+  );
+};
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,9 +32,11 @@ const queryClient = new QueryClient({
 
 const AppProvider = ({ children }: PropsWithChildren) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>{children}</BrowserRouter>
-    </QueryClientProvider>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </QueryClientProvider>
+    </ErrorBoundary>
   );
 };
 
